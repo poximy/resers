c32878e3dff2d6b11b47d64a0b18eb533ac9e80c
Erick ZV <zv.erick@protonmail.com>
2021-08-19 15:13:35 -0500

    Add table component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import Table from "./components/Table.svelte";
 </script>
 
 <main>
+  <Table/>
 </main>
 
 <style>
---
+++ src/components/Table.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let cells: number[] = Array(10).fill(0);
+
+  $: average = cells.reduce((x, y) => x + y) / 10
+</script>
+
+<ul>
+  {#each cells as _, index}
+    <li>
+      <input type="number" bind:value={cells[index]} />
+    </li>
+  {/each}
+</ul>
+<p>Average: {average}</p>
+
+<style>
+</style>
