b4e4d6622127804d30b5ceeea23ab430032e9683
Erick ZV <zv.erick@protonmail.com>
2021-08-22 16:45:26 -0500

    Move code to Table

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,10 @@
 <script lang="ts">
   import Table from "./components/Table.svelte";
-
-  let tableAverages: number[] = Array(1).fill(0);
-
-  const createTable = () => {
-    tableAverages = [...tableAverages, 0];
-  };
-
-  const deleteTable = () => {
-    tableAverages.pop();
-    tableAverages = tableAverages;
-  };
 </script>
 
 <main>
-  <table>
-    <thead>
-      <tr>
-        <th colspan="5">weights</th>
-        <th>average</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each Array(tableAverages.length) as _, index}
-        <Table bind:average={tableAverages[index]} />
-      {/each}
-    </tbody>
-  </table>
+  <Table />
 </main>
-
-<div class="buttons">
-  <button on:click={createTable}>➕</button>
-  <button on:click={deleteTable}>➖</button>
-</div>
 
 <style>
 </style>
--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,25 +1,51 @@
 <script lang="ts">
-  export let average: number;
+  let tables: number[][] = [Array(10).fill(0)];
 
-  let cells: number[] = Array(10).fill(0);
-  $: average = cells.reduce((x, y) => x + y) / 10;
+  const createTable = () => {
+    tables = [...tables, Array(10).fill(0)];
+  };
+
+  const deleteTable = () => {
+    tables.pop();
+    tables = tables;
+  };
 </script>
 
-<tr>
-  {#each Array(5) as _, index}
-    <td>
-      <input type="number" bind:value={cells[index]} />
-    </td>
-  {/each}
-  <td>{average}</td>
-</tr>
-<tr>
-  {#each Array(5) as _, index}
-    <td>
-      <input type="number" bind:value={cells[index + 5]} />
-    </td>
-  {/each}
-</tr>
+<table>
+  <thead>
+    <tr>
+      <th colspan="5">weights</th>
+      <th>average</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each Array(tables.length) as _, tableNumber}
+      <tr>
+        {#each Array(5) as _, cellNumber}
+          <td>
+            <input type="number" bind:value={tables[tableNumber][cellNumber]} />
+          </td>
+        {/each}
+        <td rowspan="2">{tables[tableNumber].reduce((x, y) => x + y) / 10}</td>
+      </tr>
+      <tr>
+        {#each Array(5) as _, cellNumber}
+          <td>
+            <input
+              type="number"
+              bind:value={tables[tableNumber][cellNumber + 5]}
+            />
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<div class="buttons">
+  <button on:click={createTable}>➕</button>
+  <button on:click={deleteTable}>➖</button>
+</div>
 
 <style>
 </style>
