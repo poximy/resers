f0286b1963e341540cfd7e45b6f15d06282f2b40
poximy <76846708+poximy@users.noreply.github.com>
2022-07-28 14:14:01 -0500

    style: add .prettierrc

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,svelte}"],
+  content: ['./index.html', './src/**/*.{html,svelte}'],
   theme: {
     extend: {},
   },
